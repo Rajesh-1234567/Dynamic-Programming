71c98bba980e9ae7c05b5872b2f3b0b551c99a07
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2023-11-11 20:55:00 +0530

    Update fibonacciByDp.java

--- fibonacciByDp.java
+++ fibonacciByDp.java
@@ -3,11 +3,11 @@ public class fibonacci{
     public static int fibo(int n, int arr[]){
         if(n==0 || n==1) return n;
         if(arr[n]!=0) return arr[n];
-        return fibo(n-1,arr)+fibo(n-2,arr);
+        return fibo(n-1,arr)+fibo(n-2,arr);                 //BY MEMONIZATION 
     }
     public static int fiboTabulation(int n, int dp[]){
         dp[0]=0;dp[1]=1;
-        for(int i=2;i<n+1;i++){
+        for(int i=2;i<n+1;i++){                            // BY TABULATION
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
