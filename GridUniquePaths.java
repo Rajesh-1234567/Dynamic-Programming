980d4a51ce8d4438d32fd6e365a10e2ef6f7e8c2
Rajesh kumar jena <129972543+Rajesh-1234567@users.noreply.github.com>
2024-03-17 16:19:59 +0000

    Unique paths in a grid done

---
+++ GridUniquePaths.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class GridUniquePaths {
+    public static int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        return find(m - 1, n - 1, dp);
+    }
+
+    public static int find(int i, int j, int[][] dp) {
+        if (i == 0 && j == 0) return 1;
+        if (i < 0 || j < 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        int left = find(i - 1, j, dp);
+        int up = find(i, j - 1, dp);
+        return dp[i][j] = left + up;
+    }
+
+    public static int tabulation(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if (i > 0)
+                    up = dp[i - 1][j];
+                if (j > 0)
+                    left = dp[i][j - 1];
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        int m = 3;
+        int n = 3;
+        System.out.println("Using recursion: " + uniquePaths(m, n));
+        System.out.println("Using tabulation: " + tabulation(m, n));
+    }
+}
